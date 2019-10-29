5b527fe0a68a081bc77c8f5b156a509a8f6cc56d
WahomeMW <mwaivanwahome@gmail.com>
2019-10-28 19:51:11 -0500

    git commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;namespace ExampleOfClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product("nintendo switch", 299.99m);
+
+            Console.WriteLine($"Your product is a J_O_Y_C_O_N_B_O_Y_Z_ {product.Name} for {product.Price}. Represent.");
+        }
+    }
+}
